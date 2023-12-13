139b138943e07c1977a2da5dcc550098f39ec270
jesuskaya <jesus.kayya@mail.ru>
2023-12-13 19:40:06 +0200

    initial commit

---
+++ homework.py
@@ -0,0 +1,37 @@
+##### 1 задание:
+
+# number1 = int(input("Enter a number: "))
+# number2 = int(input("Enter a number: "))
+# number3 = int(input("Enter a number: "))
+#
+# n1  = number1
+# n2 = number2
+# n3 = number3
+#
+# result = n1 + n2 + n3
+# result2 = n1 * n2 * n3
+# print(f"n1: {n1} n2: {n2} n3: {n3} result: {result} result2: {result2}")
+
+##### 2 Задание:
+
+# lengthD1 = int(input("Enter the length of the diagonal:"))
+# lengthD2 = int(input("Enter the length of the diagonal:"))
+#
+# LD1 = lengthD1
+# LD2 = lengthD2
+#
+# result = (LD1 + LD2) / 2
+# print(f"S = {result}")
+
+# 3 Задание(Дополнительно):
+
+# number = int(input("Enter 4-digit number: "))
+#
+# n1 = number // 1000
+# n2 = number // 100 % 10
+# n3 = number // 10 % 10
+# n4 = number % 10
+#
+# result = n1 * n2 * n3 * n4
+# print(f"n1: {n1} n2: {n2} n3 {n3} n4 {n4}")
+# print(f"Result: {result}")
