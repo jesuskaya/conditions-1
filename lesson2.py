20111edaf75487d65f52006778cc6f3d1afcc7d4
jesuskaya <jesus.kayya@mail.ru>
2023-12-13 20:59:01 +0200

    added examples

--- lesson2.py
+++ lesson2.py
@@ -12,11 +12,38 @@
 
 #
 
-print(1 == 1 and 3 == 3)  #вернёт Тру если оба оператора равны, Фолс если нет
-print(1 == 1 and 2 == 3) #вернёт Тру если хотяб один оператор равен другому, иначе нет
+# print(1 == 1 and 3 == 3)  #вернёт Тру если оба оператора равны, Фолс если нет
+# print(1 == 1 and 2 == 3) #вернёт Тру если хотяб один оператор равен другому, иначе нет
+#
+# is_valid = False
+# print(is_valid)
+# print(not is_valid) # not > инверсия, если значение Фолс станет Тру, и наоборот
+#
+# print("hello" in "hello world")
 
-is_valid = False
-print(is_valid)
-print(not is_valid) # not > инверсия, если значение Фолс станет Тру, и наоборот
 
-print("hello" in "hello world")
+####
+
+# hours = int(input("Enter hours: "))
+#
+# if 12 <= hours < 24:
+#     print("PM")
+# elif 0 <= hours < 12:
+#     print("AM")
+# else:
+#     print("Incorrect hours")
+
+###
+
+# film_rating = int(input("Enter film rating: "))
+#
+# if film_rating > 0 and film_rating <= 5:
+#     if film_rating == 4 or film_rating == 5:
+#         print("OK")
+#     else:
+#         print("Not OK")
+# else:
+#     print("Incorrect Rating")
+#
+# print("hello world")
+
