2fe90b142cad0dad5be41f14bedf6cb1cad0da7a
jesuskaya <jesus.kayya@mail.ru>
2023-12-14 15:06:11 +0200

    task 1 completed

--- homework2.py
+++ homework2.py
@@ -1,23 +1,23 @@
 #Task - 1
 
-f1 = int(input("Enter a number 1: "))
-f2 = int(input("Enter a number 2: "))
-f3 = int(input("Enter a number 3: "))
-
-print("Choose operation")
-print("1 - max")
-print("2 - min")
-print("3 - average")
-
-choice = int(input("Enter your choice: "))
-
-if choice == 1:
-    result1 = max(f1, f2, f3)
-    print("The maximum is:", {result1})
-elif choice == 2:
-    result2 = min(f1, f2, f3)
-    print("The minimum is:", {result2})
-elif choice == 3:
-    result3 = (f1 + f2 + f3) / 3
-    print("The average is:", {result3})
-else: print("Invalid!")
+# f1 = int(input("Enter a number 1: "))
+# f2 = int(input("Enter a number 2: "))
+# f3 = int(input("Enter a number 3: "))
+#
+# print("Choose operation")
+# print("1 - max")
+# print("2 - min")
+# print("3 - average")
+#
+# choice = int(input("Enter your choice: "))
+#
+# if choice == 1:
+#     result1 = max(f1, f2, f3)
+#     print("The maximum is:", {result1})
+# elif choice == 2:
+#     result2 = min(f1, f2, f3)
+#     print("The minimum is:", {result2})
+# elif choice == 3:
+#     result3 = (f1 + f2 + f3) / 3
+#     print("The average is:", {result3})
+# else: print("Invalid!")
