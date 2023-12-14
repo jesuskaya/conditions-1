2ff8d37bae5bca806580e8d2cb57de6f50792cea
jesuskaya <jesus.kayya@mail.ru>
2023-12-14 15:22:51 +0200

    task 1 completed

--- homework2.py
+++ homework2.py
@@ -22,3 +22,31 @@
 #     print("The average is:", {result3})
 # else: print("Invalid!")
 
+#Task 2
+
+meters = float(input("Enter the meters: "))
+
+print("Choose operation")
+print("1 - yards")
+print("2 - miles")
+print("3 - inches")
+
+choice = input("Operation:")
+
+yards = 0.9144
+miles = 1609.34
+inches = 0.0254
+
+if choice == "1":
+    result = meters / yards
+    print("The result is:", result)
+elif choice == "2":
+    result = meters / miles
+    print("The result is:", result)
+
+elif choice == "3":
+    result = meters / inches
+    print("The result is:", result)
+else: print("Invalid")
+
+
