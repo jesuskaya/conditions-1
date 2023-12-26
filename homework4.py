d973be3617530d35cead39c35b45042890c6c35e
jesuskaya <jesus.kayya@mail.ru>
2023-12-26 05:59:52 +0200

    Homework is done!

---
+++ homework4.py
@@ -0,0 +1,96 @@
+#### Task_1
+
+# string = input("Enter a string: ")
+#
+# letter_count = 0
+# digit_count = 0
+#
+# for i in string:
+#     if "a" <= char <= "z" or "A" <= char <= "Z":
+#         letter_count += 1
+#     elif "0" <= char <= "9":
+#         digit_count += 1
+# print("Letter count:", letter_count)
+# print("Digit count:", digit_count)
+
+#### Task_2
+
+# string = input("Enter a string: ")
+# symbol = input("Enter a symbol: ")
+#
+# symbol_count = 0
+#
+# for i in string:
+#     if i == symbol:
+#         symbol_count += 1
+#
+# print(f"The symbol {string} appears {symbol_count} times.")
+
+#### Task_3
+
+# input_string = input("Enter a string: ")
+# search_word = input("Enter a word for search: ")
+# replace_word = input("Enter a word for replace: ")
+#
+# result_string = input_string.replace(search_word, replace_word)
+#
+# print("Ur string: ", result_string)
+
+### я не уверен как это задание именно нужно было сделать, но если что я могу добавить цикл, если "пользователь" введёт что-то не то. Но если не учитывать это, то всё работает.
+
+####Task_4
+
+# string = "Hello world"
+#
+# print(string[2])
+#
+# print(string[-2])
+#
+# print(string[:5])
+#
+# print(string[:-2])
+#
+# print(string[::2])
+#
+# print(string[1::2])
+#
+# print(string[::-1])
+#
+# print(string[-1::-2])
+#
+# print(len(string))
+
+
+
+#### Task_5
+
+# text = "Hello bro, a 5 years ago i flew in to Turkey!"
+#
+#
+#
+# new_text = text.capitalize()
+#
+# digital_count = sum(char.isdigit() for char in text)
+#
+# punctuation_count = sum(char in ",.!?;:'" for char in text)
+#
+# signs_count = text.count("!")
+#
+# print("New text is:")
+# print(new_text)
+#
+# print("\nDigital count is:")
+# print(digital_count)
+#
+# print("\nPunctuation count is:")
+# print(punctuation_count)
+#
+# print("\nSigns count is:")
+# print(signs_count)
+
+
+
+
+
+
+
